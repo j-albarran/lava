6cfabcf622a9af5b4045ff042e8404840c341dd9
Josue Albarran <jalbarran@debian.wwwgate.ti.com>
2016-07-29 14:58:41 -0500

    Added wrapper script

---
+++ ltp-ddt.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import re
+import sys
+
+
+#def results():
+#   pattern = re.compile("^(?!.+ED)(?P<test_case_id>\w+)\s+(?P<result>PASS|FAIL)\s+\d+")
+#   finished = 0
+#   for line in sys.stdin:
+#       for parser in [pattern]:
+#           result = parser.search(line)
+#           if result is not None:
+#              if p
+#                 test_result = result.group
+
+lava-test-case test --result fail
