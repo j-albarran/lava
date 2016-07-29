b61a88209526e5fbd89b1a17436ecb56d4a8a55f
Josue Albarran <jalbarran@debian.wwwgate.ti.com>
2016-07-29 15:33:17 -0500

    Added wrapper script

--- ltp-ddt.py
+++ ltp-ddt.py
@@ -14,4 +14,20 @@ import os
 #              if p
 #                 test_result = result.group
 
-os.system('lava-test-case test --result fail')
+#os.system('lava-test-case test --result fail')
+
+pattern = re.compile("^(?!.+ED)(?P<test_case_id>\w+)\s+(?P<result>PASS|FAIL)\s+\d+")
+finished = 0
+for line in sys.stdin:
+    for parser in [pattern]:
+        result = parser.search(line)
+        if result is not None:
+           if parser is pattern:
+              test_id = result.group('id')
+	      test_result = result.group('result')
+              os.system('lava-test-case %s --result %s' % (test_id, test_result)
+              break
+        else:
+            continue
+    print "ERROR: Parser failed and ran to EOF"
+    sys.exit(-1)
