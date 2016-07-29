bd410343a4d7b8e8c7cd28589a2b7a44c3756e32
Josue Albarran <jalbarran@debian.wwwgate.ti.com>
2016-07-29 16:39:20 -0500

    Changed TD

--- ltp-ddt.py
+++ ltp-ddt.py
@@ -16,17 +16,20 @@ import os
 
 #os.system('lava-test-case test --result fail')
 
+os.system('cd /opt/ltp')
+os.system('./runltp -P am335x-evm -f ddt/eth_ping -s "ETH_XS_FUNC_PING” > output.txt')
+
 os.system('echo HELLO')
 pattern = re.compile("^(?!.+ED)(?P<test_case_id>\w+)\s+(?P<result>PASS|FAIL)\s+\d+")
-finished = 0
-for line in sys.stdin:
-    for parser in [pattern]:
-        result = parser.search(line)
-        if result is not None:
-           if parser is pattern:
-              test_id = result.group('id')
-	      test_result = result.group('result')
-              os.system('lava-test-case %s --result %s' % (test_id, test_result))
-              sys.exit(0)
+with open(output.txt, 'r') as f
+   for line in f:
+      for parser in [pattern]:
+          result = parser.search(line)
+          if result is not None:
+             if parser is pattern:
+                test_id = result.group('id')
+	        test_result = result.group('result')
+                os.system('lava-test-case %s --result %s' % (test_id, test_result))
+                sys.exit(0)
 print "ERROR: Parser failed and ran to EOF"
 sys.exit(1)
