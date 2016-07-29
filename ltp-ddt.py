371e0c853d930c176c292746d4f95445c3f0ac39
Josue Albarran <jalbarran@debian.wwwgate.ti.com>
2016-07-29 15:07:00 -0500

    Added wrapper script

--- ltp-ddt.py
+++ ltp-ddt.py
@@ -2,7 +2,7 @@
 
 import re
 import sys
-
+import os
 
 #def results():
 #   pattern = re.compile("^(?!.+ED)(?P<test_case_id>\w+)\s+(?P<result>PASS|FAIL)\s+\d+")
@@ -14,4 +14,4 @@ import sys
 #              if p
 #                 test_result = result.group
 
-lava-test-case test --result fail
+os.system('lava-test-case test --result fail')
