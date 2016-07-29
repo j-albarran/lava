512abe9f12275670ed53d7ea87233cf945eb0c7a
Josue Albarran <jalbarran@debian.wwwgate.ti.com>
2016-07-29 15:40:21 -0500

    Added wrapper script

--- ltp-ddt.py
+++ ltp-ddt.py
@@ -25,9 +25,7 @@ for line in sys.stdin:
            if parser is pattern:
               test_id = result.group('id')
 	      test_result = result.group('result')
-              os.system('lava-test-case %s --result %s' % (test_id, test_result)
-              break
-        else:
-            continue
-    print "ERROR: Parser failed and ran to EOF"
-    sys.exit(-1)
+              os.system('lava-test-case %s --result %s' % (test_id, test_result))
+              sys.exit(0)
+print "ERROR: Parser failed and ran to EOF"
+sys.exit(1)
