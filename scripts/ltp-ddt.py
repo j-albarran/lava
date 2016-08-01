ec284cdfc24cefb908174145a338347f25a5138a
Josue Albarran <jalbarran@debian.wwwgate.ti.com>
2016-08-01 12:03:01 -0500

    Changed script

--- scripts/ltp-ddt.py
+++ scripts/ltp-ddt.py
@@ -3,6 +3,7 @@
 import re
 import sys
 import os
+import subprocess
 
 #def results():
 #   pattern = re.compile("^(?!.+ED)(?P<test_case_id>\w+)\s+(?P<result>PASS|FAIL)\s+\d+")
@@ -16,8 +17,8 @@ import os
 
 #os.system('lava-test-case test --result fail')
 
-os.system("cd /opt/ltp")
-os.system('./runltp -P am335x-evm -f ddt/eth_ping -s "ETH_XS_FUNC_PING" > output.txt')
+subprocess.call("cd /opt/ltp")
+subprocess.call('./runltp -P am335x-evm -f ddt/eth_ping -s "ETH_XS_FUNC_PING" > output.txt')
 
 os.system('echo HELLO')
 pattern = re.compile("^(?!.+ED)(?P<test_case_id>\w+)\s+(?P<result>PASS|FAIL)\s+\d+")
