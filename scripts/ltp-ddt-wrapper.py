37128dd33b758026fd920f1ebbad02021a80ba8b
Josue Albarran <jalbarran@debian.wwwgate.ti.com>
2016-08-02 13:48:33 -0500

    Changed script

--- scripts/ltp-ddt-wrapper.py
+++ scripts/ltp-ddt-wrapper.py
@@ -5,8 +5,8 @@ import sys
 import os
 import subprocess
 
-os.system('/opt/ltp/runltp -P am335x-evm -f ddt/eth_ping -s "ETH_XS_FUNC_PING" > output.txt')
-os.system('/opt/ltp/runltp -P am335x-evm -f ddt/emmc_dd_rw_ext2 -s "EMMC_S_FUNC_EXT2_DD_RW_100M" >> output.txt')
+os.system('/opt/ltp/runltp -P am335x-evm -f ddt/eth_ping -s "ETH_XS_FUNC_PING" 2>&1 | tee output.txt')
+os.system('/opt/ltp/runltp -P am335x-evm -f ddt/emmc_dd_rw_ext2 -s "EMMC_S_FUNC_EXT2_DD_RW_100M" 2>&1 | tee -a output.txt')
 
 pattern = re.compile("^(?!.+ED)(?P<test_case_id>\\w+)\\s+(?P<result>PASS|FAIL)\\s+\\d+")
 f = open("output.txt", "r")
